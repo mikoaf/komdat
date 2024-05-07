d9f822c7307e2a63b8f8137fbd87f44d1acf57df
nanangcreacoll <nanang.creacol.26@gmail.com>
2024-05-07 10:08:25 +0700

    read dht11 sensor

--- sensor/dht.py
+++ sensor/dht.py
@@ -3,9 +3,19 @@ from model.dht import DhtResult
 from model.response import Response, StatusResponse
 from sensor.dhtbase import DhtBase
 import asyncio
+import adafruit_dht
+import board
 
 class Dht(DhtBase):
+    def __init__(self) -> None:
+        self.dht11_sensor = adafruit_dht.DHT11(board.D26)
     async def getData(self, msg: str) -> Tuple[Response, DhtResult]:
-        lah = 8.9
-        leh = 32
-        return (Response(status=StatusResponse.success,message="success"), DhtResult(humidity=lah, temp=leh))
+        try:
+            hum = self.dht11_sensor.humidity
+            temp = self.dht11_sensor.temperature
+            return (Response(status=StatusResponse.success,message="success"), DhtResult(humidity=hum, temp=temp))
+        except RuntimeError as e:
+            return (Response(status=StatusResponse.failed,message=str(e)), DhtResult(humidity=0, temp=0))
+        except Exception as e:
+            self.dht11_sensor.exit()
+            raise e
